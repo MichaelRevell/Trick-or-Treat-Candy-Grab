4f5262cebab1009608d8e058cc743ee2615ba7dd
Michael Revell <mikearevell@gmail.com>
2013-10-16 23:46:35 -0700

    Initial Commit

---
+++ Trick or Treat Candy Grab/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Trick or Treat Candy Grab
+//
+//  Created by Michael Revell on 10/16/13.
+//  Copyright (c) 2013 Michael Revell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Trick or Treat Candy GrabTests/Trick_or_Treat_Candy_GrabTests.m
@@ -0,0 +1,34 @@
+//
+//  Trick_or_Treat_Candy_GrabTests.m
+//  Trick or Treat Candy GrabTests
+//
+//  Created by Michael Revell on 10/16/13.
+//  Copyright (c) 2013 Michael Revell. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Trick_or_Treat_Candy_GrabTests : XCTestCase
+
+@end
+
+@implementation Trick_or_Treat_Candy_GrabTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
